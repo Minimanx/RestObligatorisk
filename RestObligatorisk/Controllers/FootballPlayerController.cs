ba767618a8ee4960a59a13701a7919567a8ecea3
Carl Johan Cederskog Schwennesen <carl170g@edu.easj.dk>
2021-10-11 09:12:22 +0200

    Add project files.

---
+++ RestObligatorisk/Controllers/FootballPlayerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary;
+using RestObligatorisk.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestObligatorisk.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballPlayerController : ControllerBase
+    {
+        private readonly FootballPlayerManager _manager = new FootballPlayerManager();
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public FootballPlayer Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public FootballPlayer Post([FromBody] FootballPlayer value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public FootballPlayer Put(int id, [FromBody] FootballPlayer value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public FootballPlayer Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ RestObligatorisk/Managers/FootballPlayerManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace RestObligatorisk.Managers
+{
+    public class FootballPlayerManager
+    {
+        private static int _nextId = 1;
+
+        private static readonly List<FootballPlayer> Players = new List<FootballPlayer>
+        {
+            new FootballPlayer() {Id = _nextId++, Name = "Jørgen", Price = 999, ShirtNumber = 20}
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(Players);
+        }
+
+        public FootballPlayer GetById(int id)
+        {
+            return Players.Find(player => player.Id == id);
+        }
+
+        public FootballPlayer Add(FootballPlayer newPlayer)
+        {
+            newPlayer.Id = _nextId++;
+            Players.Add(newPlayer);
+            return newPlayer;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer player = Players.Find(playerToDelete => playerToDelete.Id == id);
+            if (player == null) return null;
+            Players.Remove(player);
+            return player;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer updatedPlayer)
+        {
+            FootballPlayer player = Players.Find(playerToUpdate => playerToUpdate.Id == id);
+            if (player == null) return null;
+            player.Name = updatedPlayer.Name;
+            player.Price = updatedPlayer.Price;
+            player.ShirtNumber = updatedPlayer.ShirtNumber;
+            return player;
+        }
+    }
+}
